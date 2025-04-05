11c8189bf3f2dd2a9f4e779ec7059e8a096e3713
homesickalien <xuanwomaodan@gmail.com>
2025-04-05 19:13:46 +0800

    data transimission

---
+++ serialport/serialport.h
@@ -0,0 +1,44 @@
+#ifndef SERIALPORT_H
+#define SERIALPORT_H
+
+#include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
+#include <boost/asio/serial_port.hpp>
+
+using namespace boost::asio;
+using namespace std;
+using namespace boost::placeholders;
+
+class MySerial{
+private:
+    string port_name;
+    unsigned int baud_rate;
+    unsigned int character_size;
+    serial_port sp;
+    int count=0;
+ 
+    
+public:
+    uint8_t angle;
+    int8_t position=75;
+    
+    MySerial(string name,unsigned int baud_rate,unsigned int character_size,io_context &io);
+
+    bool init_and_open();
+
+    void flush_buffer();
+
+    void my_async_read();
+
+    void my_async_write();
+
+    void write_callback(const boost::system::error_code &ec,size_t bytes);
+
+    void read_callback(const boost::system::error_code &ec,size_t bytes);
+
+    void close_port();
+
+    
+};
+
+#endif
